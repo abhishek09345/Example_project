fa88dc4516a8584cda1d7559f1aab1f55178c4f4
Abhishek Patil <abhishekap.ap81@gmail.com>
2021-05-28 13:38:39 +0530

    Added Prime number C file

---
+++ Prime number/main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+int arr[20],l,h,i,j,k=0;
+
+printf("\n enter the lowest and highest number of he range\n");
+scanf("%d%d",&l,&h);
+
+for(i=l;i<=h;i++)
+{int flag=0;
+
+    for(j=2;j<i/2;j++)
+    {
+        if(i%j==0)
+        {
+            flag=1;
+            break;
+        }
+
+    }
+    if(flag==1)
+    continue;
+
+    arr[k++]=i;
+}
+
+
+
+for(i=0;i<k;i++)
+printf("\nThe prime numbers are %d\n",arr[i]);
+
+return 0;
+
+}
